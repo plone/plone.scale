d4443d4fa6b102766a4a9b8c47e07990b6d4ab36
Wichert Akkerman <wichert@wiggy.net>
2009-08-08 20:54:23 +0000

    Start base storage implementation
    svn path=/plone.scale/trunk/; revision=28529

---
+++ plone/scale/tests/test_storage.py
@@ -0,0 +1,57 @@
+import operator
+import unittest
+
+
+class BaseAnnotationStorageTests(unittest.TestCase):
+    def createStorage(self):
+        from plone.scale.storage import BaseAnnotationStorage
+        storage=BaseAnnotationStorage()
+        storage.fieldname="fieldname"
+        storage.annotations={}
+        return storage
+
+    def testGetItemWithoutAnnotations(self):
+        storage=self.createStorage()
+        self.assertRaises(KeyError, operator.itemgetter("key"), storage)
+
+    def testIterateWithoutAnnotations(self):
+        storage=self.createStorage()
+        self.assertEqual(list(storage), [])
+
+    def testIterate(self):
+        import types
+        storage=self.createStorage()
+        storage.annotations["plone.scale.fieldname"]=[("one", None), ("two", None)]
+        generator=iter(storage)
+        self.failUnless(isinstance(generator, types.GeneratorType))
+        self.assertEqual(list(generator), ["one", "two"])
+
+    def testKeys(self):
+        storage=self.createStorage()
+        storage.annotations["plone.scale.fieldname"]=[("one", None), ("two", None)]
+        self.failUnless(isinstance(storage.keys(), list))
+        self.assertEqual(storage.keys(), ["one", "two"])
+
+    def testNegativeHasKey(self):
+        storage=self.createStorage()
+        self.assertEqual(storage.has_key("one"), False)
+
+    def testPositiveHasKey(self):
+        storage=self.createStorage()
+        storage.annotations["plone.scale.fieldname.one"]=None
+        self.assertEqual(storage.has_key("one"), True)
+
+    def testDeleteNonExistingItem(self):
+        storage=self.createStorage()
+        self.assertRaises(KeyError, operator.delitem, storage, "key")
+
+    def testDeleteRemovesItemAndIndex(self):
+        storage=self.createStorage()
+        storage.annotations["plone.scale.fieldname"]=[("key", None), ("other", None)]
+        storage.annotations["plone.scale.fieldname.key"]=None
+        storage.annotations["plone.scale.fieldname.other"]=None
+        del storage["key"]
+        self.assertEqual(storage.annotations["plone.scale.fieldname"], [("other", None)])
+        self.failUnless("plone.scale.fieldname.key" not in storage.annotations)
+
+
