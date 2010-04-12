d4dd9d48102b4a5c505cca6efb226522eceffb53
Andreas Zeidler <witsch@plone.org>
2010-04-12 15:50:28 +0000

    bump version
    svn path=/plone.scale/trunk/; revision=36049

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 from setuptools import setup, find_packages
 
-version = "1.0"
+version = "1.1"
 readme = open("README.txt").read().replace(':class:', '').replace(':mod:', '')
 changes = open(join("docs", "changes.rst")).read()
 
