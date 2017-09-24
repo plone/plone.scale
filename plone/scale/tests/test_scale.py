4ece9b55c80b87fa9ea1d690ff3b56e0facbfd49
Florian Schulze <florian.schulze@gmx.net>
2017-09-24 10:56:26 +0200

    Don't turn warnings into errors, this seems to leak into other Plone tests.

--- plone/scale/tests/test_scale.py
+++ plone/scale/tests/test_scale.py
@@ -257,7 +257,4 @@ class ScalingTests(TestCase):
 
 def test_suite():
     from unittest import defaultTestLoader
-    from warnings import filterwarnings
-    filterwarnings("error", "the 'direction' option is deprecated")
-    filterwarnings("error", "the '.*' scaling mode is deprecated")
     return defaultTestLoader.loadTestsFromName(__name__)
