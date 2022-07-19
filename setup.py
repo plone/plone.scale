08f276f75fd9bbf3a84a1aa896918902556c3ef5
Maurits van Rees <maurits@vanrees.org>
2022-07-19 16:31:35 +0200

    Back to development: 4.0.0b4
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "4.0.0b3"
+version = "4.0.0b4.dev0"
 with open("README.rst") as myfile:
     readme = myfile.read()
 with open("CHANGES.rst") as myfile:
